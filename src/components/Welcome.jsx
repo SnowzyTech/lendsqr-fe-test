e0d180794f83b95c803c27e05e9a00765abce206
DevEzenwa <jesseezenwa911@gmail.com>
2023-02-04 07:47:09 -0800

    first commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = () => {
+    return(
+        <div>
+           <h1>Welcome</h1>
+           <p></p>
+        </div>
+    )
+}
+
+export default Welcome
