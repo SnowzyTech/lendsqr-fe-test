cbf040bd92c19ec65e537aeeaa23ffe7f2767cd8
ihekuna Emmanuel <ihekunaemmanuelc@gmail.com>
2023-02-14 22:08:20 +0100

    Updated Pages

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
            <input className='input' type="email" placeholder='Email'></input>
            <input className='input' type="password" placeholder='Password'></input>
             <a href='#'>forgot password?</a>
-            <Link to="/home">
+            <Link to="/home" className="linkbtn">
               <button className='btn'>log in</button>
             </Link>
         </div>
